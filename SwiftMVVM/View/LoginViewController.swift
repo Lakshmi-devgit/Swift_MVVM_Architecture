c220cb4f5d20d051275ec1bef8714d99cbb33b42
Lakshmi <lakshmi@Lakshmis-MacBook-Pro.local>
2019-08-29 05:06:42 +0530

    fitst commit

---
+++ SwiftMVVM/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMVVM
+//
+//  Created by Lakshmi on 29/08/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SwiftMVVM/ViewModel/LoginVIewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginVIewModel.swift
+//  SwiftMVVM
+//
+//  Created by Lakshmi on 29/08/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import Foundation
